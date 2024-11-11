d38075881d46f5bafe45ef441e34cadccfdafbde
shmuel keller <shmuli@shmuels-Air.attlocal.net>
2024-11-11 02:29:33 -0500

    added note

---
+++ Note/create.ts
@@ -0,0 +1,20 @@
+import { createMutable } from "solid-js/store";
+
+import type { NoteProps } from "./types";
+
+
+
+export default function create_note(localStorageName : string = "note") {
+    if (localStorage.getItem(localStorageName) !== null) {
+        return JSON.parse(localStorage.getItem(localStorageName) as string) as NoteProps;
+    }
+    return createMutable<NoteProps>({
+    title: "",
+    content: "",
+    id: 0,
+    color: "orange",
+    localStorageName: localStorageName,
+    position: { x: 0, y: 0 },
+    unsaved: false
+})
+}
